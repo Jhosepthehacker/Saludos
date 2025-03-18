daadedf570170a2ced510d69ec28f869f8f21e03
Jhosepthehacker <josemanuelramirezrengifo9@gmail.com>
2025-03-18 15:02:03 -0400

    Create Saludos.py

---
+++ Saludos.py
@@ -0,0 +1,22 @@
+#python 3.7.1
+
+#Entrada de datos
+
+nombre = input ("¿Cómo te llamas?: ")
+
+#Saludos
+
+print (f"Hola {nombre} Bienvenido(a) a python :) ^_^")
+
+#Contáctame
+
+contacto = input ("¿Quiéres contactarte conmigo?")
+
+#Comprobando al respuesta del usuario
+
+if {contacto}>=si
+print ("Correo: josemanuelramirezrengifo9@gmail.com")
+
+print ("")
+
+print ("Por favor usar está información solo para contactarse")
